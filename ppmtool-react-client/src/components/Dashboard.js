f6dc097cab0fc53c52f37401e3024b90c36ced60
Frank Martin <cedricmrtn@gmail.com>
2020-03-16 06:50:19 -0500

    Stub Header, Project items, and dashboard components

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import Header from "./Layout/Header";
 
 export default class Dashboard extends Component {
   render() {
     return (
-        
-    )
+      <div>
+        <Header />
+        <h1>Welcome to the Dashboard</h1>
+        <ProjectItem />
+      </div>
+    );
   }
 }
-
-export default Dashboard;
